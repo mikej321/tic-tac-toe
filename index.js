a94013194d0d5a2e0ab178bb0494962c84a25895
Michael Johnson <michaeljcantley@gmail.com>
2023-08-14 18:21:12 -0400

    finish javascript functionality

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ const myGameboard = (() => {
             square.textContent = gameBoard[i];
             gameBoardEl.insertAdjacentElement('beforeend', square);
         }
+        
     }
 
+    // Changes the text in the tic-tac-toe every turn to match the gameBoard Array
     let reRender = function(arr) {
         const squares = document.querySelectorAll('.square');
         for (let i = 0; i < squares.length; i++) {
@@ -32,9 +34,28 @@ const myGameboard = (() => {
     let pickASquare = function(event) {
         if (!event.target.hasAttribute('occupied') && event.target.classList.contains('square')) {
             playerChoice();
+            reRender(gameBoard);
+            // if the board is full and a winner isn't declared by the last turn (a human will be controlling at this point), will declare a draw
+            if (isBoardFull(gameBoard) && !checkLine()) {
+                return declareDraw();
+            }
+
+            // if the human player passes the checkLine function, they are declared the winner
+            if (checkLine()) {
+                gameBoardEl.classList.add('stop');
+               return declareWinner();
+            }
+
+            // proceeds to the computer if a winner isn't declared before and checks to see if they win afterwards
             computerChoice(gameBoard);
+            reRender(gameBoard);
+
+            if (checkLine()) {
+               return declareLoser();
+            }
         }
-        reRender(gameBoard);
+
+
     }
     
     // changes the arr to fill it with the content of the picked squares
@@ -43,7 +64,8 @@ const myGameboard = (() => {
         const index = myElement.getAttribute('data-square');
         arr[index] = 'O';
     }
-    
+
+    // allows the computer to make a random choice as long as that spot in the array isn't taken 
     let computerChoice = function(arr) {
         let cpu = 'X';
         let randomIndex;
@@ -54,6 +76,7 @@ const myGameboard = (() => {
         arr[randomIndex] = cpu;
     }
     
+    // changes the css of the element that a player clicks and fills the array
     let playerChoice = function() {
         let element = event.target;
         if (!element.hasAttribute('occupied') && element.classList.contains('square')) {
@@ -63,8 +86,99 @@ const myGameboard = (() => {
         }
     }
 
+    // Conditionals that check the horizontal path first for match across all 3 lines 
+    let checkHorizontal = function(arr) {
+        let flag = false;
+        for (let i = 0; i < arr.length; i += 3) {
+            if (arr[i] === 'O' && arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]) {
+                flag = true;
+                return flag;
+            } else if (arr[i] === 'X' && arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]) {
+                flag = true;
+                return flag;
+            }
+        }
+    }
 
+    // Conditionals that check the vertical path only on the first line as that's all that's needed
+    let checkVertical = function(arr) {
+        let flag = false;
+        for (let i = 0; i < 3; i++) {
+            if (arr[i] === 'O' && arr[i] === arr[i + 3] && arr[i + 3] === arr[i + 6]) {
+                flag = true;
+                return flag;
+            } else if (arr[i] === 'X' && arr[i] === arr[i + 3] && arr[i + 3] === arr[i + 6]) {
+                flag = true;
+                return flag;
+            }
+        }
+    }
 
+    // Conditionals that check the diagonal path only on the first line as that's all that's needed
+    let checkDiagonal = function(arr) {
+        let i = 0;
+        let flag = false;
+        if (arr[i] === 'O' && arr[i] === arr[i + 4] && arr[i + 4] === arr[i + 8]) {
+            flag = true;
+            return flag;
+        } else if (arr[i + 2] === 'O' && arr[i + 2] === arr[i + 4] && arr[i + 4] === arr[i + 6]) {
+            flag = true;
+            return flag;
+        } else if (arr[i] === 'X' && arr[i] === arr[i + 4] && arr[i + 4] === arr[i + 8]) {
+            flag = true;
+            return flag;
+        } else if (arr[i + 2] === 'X' && arr[i + 2] === arr[i + 4] && arr[i + 4] === arr[i + 6]) {
+            flag = true;
+            return flag;
+        }
+    }
+
+    // Utilizes the check helper functions to check if a match is found whether it's through the player or AI
+    let checkLine = function() {
+        let flag = false;
+        if (checkHorizontal(gameBoard) || checkVertical(gameBoard) || checkDiagonal(gameBoard)) {
+            flag = true;
+        }
+        return flag;
+    }
+
+    // helper function that tests for a full array
+    let testForFullArr = function(element) {
+        return element !== '';
+    }
+
+    // tests if the board is full via a helper function and a flag
+    let isBoardFull = function(arr) {
+        let isFull = false;
+        if (arr.every(testForFullArr)) {
+            isFull = true;
+        }
+    }
+
+    // the declare functions generate DOM elements based on if a winner, loser, or a draw occurs
+    let declareWinner = function() {
+        const winner = document.createElement('h1');
+        winner.classList.add('winner');
+        winner.textContent = 'You win!';
+        gameBoardEl.insertAdjacentElement('afterend', winner);
+    }
+
+    let declareLoser = function() {
+        const loser = document.createElement('h1');
+        loser.classList.add('loser');
+        loser.textContent = 'You lose!';
+        gameBoardEl.insertAdjacentElement('afterend', loser);
+    }
+
+    let declareDraw = function() {
+        const draw = document.createElement('h1');
+        draw.classList.add('draw');
+        draw.textContent = 'It\'s a draw!';
+        gameBoardEl.insertAdjacentElement('afterend', draw);
+    }
+
+    
+    
 
     return {
         renderBoard,
@@ -117,10 +231,11 @@ function resetNameInput() {
 
 let player1;
 
-/* Tomorrow, I need to create a conditional function that checks if a winner is found by matching characters across the arr
-and if it does, it will return a winner by creating an element below the board that states that either the player or the CPU has won. */
+
 
 
 playButton.addEventListener('click', createPlayer);
-document.addEventListener('click', myGameboard.pickASquare);
+gameBoardEl.addEventListener('click', myGameboard.pickASquare);
+
+
 
